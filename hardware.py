795079c5f88c9272bc95711748521f6d4c4951da
DarkTJ <tjmilter@gmx.de>
2019-02-27 20:47:25 +0100

    Beschreibung der Aufgaben in main.py

--- hardware.py
+++ hardware.py
@@ -1,24 +1,11 @@
-import time
-import board
-import neopixel
 
 
-ORDER = neopixel.GRB  //Die LEDS brachen Grün Rot Blau reinfolge
 
-pixels = neopixel.NeoPixel(board.D18, 7, brightness=0.2, auto_write=False,pixel_order = ORDER)
 
-while True:
 
-    pixels.fill((255, 0, 0)) //Grün
+def LEDAnzeigen:
+    for y in range(height):                                                         //height ist fest und gegeben durch die leds
+        y3 = y * 3
+        pixels[x] = (column[x][y3], column[x][y3 + 1], column[x][y3 + 2])
+
     pixels.show()
-    time.sleep(1)
-
-
-    pixels.fill((0, 255, 0)) //Rot
-    pixels.show()
-    time.sleep(1)
-
-
-    pixels.fill((0, 0, 255)) //Blau
-    pixels.show()
-    time.sleep(1)
--- main.py
+++ main.py
@@ -1,14 +1,36 @@
 //main wir vom arduino gestartet wenn eingetragen in ect. Von hier aus kann/wird alles ausgeführt
 
+
+import time
+import board
+import neopixel
 import LED_Test
+import hardware
+
+
+
+pixels = neopixel.NeoPixel(board.D18, 7, brightness=0.2, auto_write=False,pixel_order = neopixel.GRB)
 
 
 
 
 
+LED_Test.LEDTest(1)
+
+//wenn der Knopf dann Bild Umwandeln und speichern
+
+//wenn Muster dann Muster speichern
+//gespeichert wir in der column[x], x ist die breite. Für jede x ist ein Array, dieses ist höhe*3 groß sodas 0,1,2 Grün Rot Blau werte für ersten pixel sind. 3,4,5 für den nächsten usw.
 
 
-LedTest(1)
+
+
+//Variablen wie "Reihendauer" einlesen
+
+//Anzeigen auf Knopfdruck
+
+hardware.LEDAnzeigen()
+time.sleep(ReihendauerPause?!)
 
 
 
