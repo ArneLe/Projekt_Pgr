1d11eb3a43b3d03e150899041ba45f1311378f8c
DarkTJ <tjmilter@gmx.de>
2019-03-10 14:11:13 +0100

    Led anzeoigen repariert3

--- hardware.py
+++ hardware.py
@@ -8,8 +8,7 @@ def LEDAnzeigen(column, width, height,pixels,zeitderPause):
     for x in range(width):
         for y in range(height):                                                         #height ist fest und gegeben durch die leds
             y3 = y * 3
-            pixels[x] = (column[x][y3], column[x][y3 + 1], column[x][y3 + 2])
-            #pixels[x] = (255, 0, y + 70)
+            pixels[y] = (column[x][y3], column[x][y3 + 1], column[x][y3 + 2])
 
         pixels.show()
         time.sleep(zeitderPause)
