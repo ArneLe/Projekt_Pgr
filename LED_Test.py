43e143410f19dcf8704ce5afdad4ca5f6f6f93dc
DarkTJ <tjmilter@gmx.de>
2019-03-10 15:28:45 +0100

    TEst auf schwrz am ende

--- LED_Test.py
+++ LED_Test.py
@@ -6,10 +6,7 @@ def LEDTest(anzahl, pixels):
 
     while n < anzahl:
 
-        pixels.fill((255, 0, 0))
-        pixels[4] = (255, 0, 70)
-        pixels[6] = (255, 0,  70)
-        pixels[18] = (255, 0,  150)   #Gruen
+        pixels.fill((255, 0, 0))   #Gruen
         pixels.show()
         time.sleep(1)
 
@@ -33,5 +30,8 @@ def LEDTest(anzahl, pixels):
         pixels.show()
         time.sleep(1)
         n = n + 1
+
+        pixels.fill((0, 0, 0))
+        pixels.show()
 
 
